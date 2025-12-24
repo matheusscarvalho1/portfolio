7425b72a576bf1b5055c58e03a0bca3ccc8f6006
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-12-24 17:19:09 -0400

    feat: Vite image optimization setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,10 +13,33 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+     ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      exclude: undefined,
+      include: undefined,
+      includePublic: true,
+      logStats: true,   
+      ansiColors: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          { name: 'removeViewBox' },
+          { name: 'sortAttrs' },
+        ],
+      },
+      png: { quality: 80 },
+      jpeg: { quality: 75 },
+      jpg: { quality: 75 },
+      webp: {  quality: 80 },
+      avif: {  quality: 70 },
+    }),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+  chunkSizeWarningLimit: 1000,
+}
 })
