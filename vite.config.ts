550b204ad40c422757e883bec104e3560c0c6a1d
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-12-31 14:33:48 -0400

    Fix: Vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
